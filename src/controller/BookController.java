c60da0e48bb7925c00af9cb0bbeba3b99782c34b
YingkaiFu <Yingkai.Fu@hotmail.com>
2018-11-28 10:12:44 +0800

    增加实验7内容

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -20,23 +20,23 @@ public class BookController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        BookService bookService = new BookService();
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        List<Book> bookList = bookService.getBookByCategoryID(id);
-//        request.setAttribute("books",bookList);
-//        request.getRequestDispatcher("/main.jsp").forward(request,response);
-
-
         BookService bookService = new BookService();
         int id = Integer.parseInt(request.getParameter("id"));
         List<Book> bookList = bookService.getBookByCategoryID(id);
-//        request.setAttribute("books",bookList);
-//        request.getRequestDispatcher("/main.jsp").forward(request,response);
-        response.setCharacterEncoding("UTF-8");
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonStr = mapper.writeValueAsString(bookList);
-        PrintWriter out = response.getWriter();
-        out.write(jsonStr);
+        request.setAttribute("books",bookList);
+        request.getRequestDispatcher("/main.jsp").forward(request,response);
+
+
+//        BookService bookService = new BookService();
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        List<Book> bookList = bookService.getBookByCategoryID(id);
+////        request.setAttribute("books",bookList);
+////        request.getRequestDispatcher("/main.jsp").forward(request,response);
+//        response.setCharacterEncoding("UTF-8");
+//        ObjectMapper mapper = new ObjectMapper();
+//        String jsonStr = mapper.writeValueAsString(bookList);
+//        PrintWriter out = response.getWriter();
+//        out.write(jsonStr);
 
 
     }
