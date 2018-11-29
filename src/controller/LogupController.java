f8ab7d653c675f0a68524af3f5206e40459a1d81
Lfhx14550 <1455077652@qq.com>
2018-11-29 21:02:31 +0800

    更新了登录模块，不过登陆后的跳转页面还没写不知道往哪儿跳

--- src/controller/LogupController.java
+++ src/controller/LogupController.java
@@ -3,11 +3,13 @@ package controller;
 import service.UserService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/LogupController")
 public class LogupController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
