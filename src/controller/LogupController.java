0133a0e6c680ec380bff9c160d4d19a3f281bb26
YingkaiFu <Yingkai.Fu@hotmail.com>
2018-11-29 17:11:35 +0800

    完善了注册模块

--- src/controller/LogupController.java
+++ src/controller/LogupController.java
@@ -3,13 +3,11 @@ package controller;
 import service.UserService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/getBookByCategoryID")
 public class LogupController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -19,10 +17,12 @@ public class LogupController extends HttpServlet {
         Boolean status = userService.logup(username, pwd);
         if (status) {
             System.out.print("注册成功!");
+            request.getRequestDispatcher("/login.html").forward(request, response);
         } else {
             System.out.print("注册失败");
+            response.sendRedirect("/logup.html");
         }
-        request.getRequestDispatcher("/login.html").forward(request, response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
