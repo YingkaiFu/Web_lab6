34b13ebd3af81e7895092ca3161d05385f09887c
YingkaiFu <Yingkai.Fu@hotmail.com>
2018-12-02 21:26:15 +0800

    增加mainpage为登陆后界面

--- src/controller/CategoryController.java
+++ src/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import service.CategoryService;
 import vo.Category;
 
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/CategoryController")
@@ -14,9 +16,16 @@ public class CategoryController extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+//        CategoryService categoryService = new CategoryService();
+//        List<Category> list = categoryService.findAll();
+//        request.setAttribute("categories",list);
+//        request.getRequestDispatcher("/main.jsp").forward(request,response);
         CategoryService categoryService = new CategoryService();
         List<Category> list = categoryService.findAll();
-        request.setAttribute("categories",list);
-        request.getRequestDispatcher("/main.jsp").forward(request,response);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonStr = mapper.writeValueAsString(list);
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write(jsonStr);
     }
 }
---
+++ web/cart.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Yingkai
+  Date: 2018/12/2
+  Time: 14:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>购物车</title>
+</head>
+<body>
+
+</body>
+</html>
