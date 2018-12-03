c281fdbb95ed77e9c1c03f61f207d79b5b71e7b8
heyhwi <huweichu@gmail.com>
2018-12-03 15:33:30 +0800

    增加注册非空提示，合并重复代码

--- src/controller/LogupController.java
+++ src/controller/LogupController.java
@@ -31,15 +31,17 @@ public class LogupController extends HttpServlet {
         PrintWriter out = response.getWriter();
         if (username.isEmpty()|| pwd.isEmpty()){
             out.print("<script type='text/javascript' language='javascript' charset='gb2312'>alert('用户名与密码不为空');");
+            out.print("location.href='/logup.jsp';");
         } else{
 
             if (status) {
                 out.print("<script type='text/javascript' language='javascript' charset='gb2312'>alert('注册成功');");
+                out.print("location.href='/login.jsp';");
             } else {
                 out.print("<script type='text/javascript' language='javascript' charset='gb2312'>alert('注册失败');");
+                out.print("location.href='/logup.jsp';");
             }
         }
-        out.print("location.href='/login.jsp';");
         out.print("</script>");
 
 
