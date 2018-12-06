831d9da614d2ec992b5d2acb2fcdaf2600d4135f
heyhwi <huweichu@gmail.com>
2018-12-06 20:01:32 +0800

    update GetOrderByUserController and OrderService

--- src/controller/GetOrderByUserController.java
+++ src/controller/GetOrderByUserController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.org.apache.xpath.internal.operations.Or;
 import dao.OrderDao;
 import dao.impl.OrderDaoImpl;
 import service.OrderService;
 import vo.Order;
+import vo.OrderItem;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GetOrderByUserController extends HttpServlet {
@@ -20,8 +24,26 @@ public class GetOrderByUserController extends HttpServlet {
 
     }
     //by User
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-//        TODO
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        int user_id = (int)session.getAttribute("user_id");
+        OrderService orderService = new OrderService();
+        List<Order> userOrderList = orderService.getOrderByUserId(user_id);
+
+        StringBuilder jsonStr = null;
+        response.setCharacterEncoding("UTF-8");
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (Order anUserOrderList : userOrderList) {
+            int order_id = anUserOrderList.getId();
+            List<OrderItem> userOrderItemList = orderService.getOrderItemByOrderId(order_id);
+            String jsonStrTemp = mapper.writeValueAsString(userOrderItemList);
+            jsonStr.append(jsonStrTemp);
+        }
+
+        PrintWriter out = response.getWriter();
+        System.out.println(jsonStr);
+        out.write(jsonStr.toString());
 
     }
 }
--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,8 +1,11 @@
 package service;
 
 import dao.OrderDao;
+import dao.OrderItemDao;
 import dao.impl.OrderDaoImpl;
+import dao.impl.OrderItemDaoImpl;
 import vo.Order;
+import vo.OrderItem;
 
 import java.sql.SQLException;
 import java.util.Date;
@@ -10,10 +13,13 @@ import java.util.List;
 
 public class OrderService {
     private OrderDao order = new OrderDaoImpl();
+    private OrderItemDao orderItem = new OrderItemDaoImpl();
+
     public List<Order> getOrderByUserId(int id){
         return order.getOrderByUserId(id);
     }
     public List<Order> queryAll(){ return order.queryAll(); }
+    public List<OrderItem> getOrderItemByOrderId(int order_id) { return orderItem.getOrderItemByOrderId(order_id);}
     public Order getOrderById(int id) throws SQLException {
         return order.getOrderById(id);
     }
